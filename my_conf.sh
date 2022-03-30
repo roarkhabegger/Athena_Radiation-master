ae8e292886774309407d7c3c0e2abb5096d2fe76
roarkhabegger <roarkhabegger@gmail.com>
2022-03-30 16:14:25 -0500

    update makefiles

--- my_conf.sh
+++ my_conf.sh
@@ -1,6 +1,11 @@
 python3 configure.py \
  --prob=ParkerInst \
- --nghost=3 \
+ --nghost=2 \
  -b \
  -cr \
- -hdf5 
+ -hdf5 \
+ -mpi \
+ --nscalars=2 \
+ --mpiccmd=h5pcc \
+ --cflag='-DH5_HAVE_PARALLEL -lstdc++' \
+ --cxx='g++-simd'
