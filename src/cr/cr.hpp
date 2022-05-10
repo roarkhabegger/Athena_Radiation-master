861a69250eb2741bb3e1fc99620999a114bbb3ed
Roark Habegger <roarkhabegger@gmail.com>
2022-05-10 16:34:34 -0500

    variable vmax

--- src/cr/cr.hpp
+++ src/cr/cr.hpp
@@ -36,32 +36,33 @@ class CosmicRay {
 public:
   CosmicRay(MeshBlock *pmb, ParameterInput *pin);
 //  ~CosmicRay();
-    
+
   AthenaArray<Real> u_cr, u_cr1, u_cr2; //cosmic ray energy density and flux
 
   AthenaArray<Real> coarse_cr_;
 
   //   diffusion coefficients for both normal diffusion term, and advection term
-  AthenaArray<Real> sigma_diff, sigma_adv; 
+  AthenaArray<Real> sigma_diff, sigma_adv;
 
   AthenaArray<Real> v_adv; // streaming velocity
   AthenaArray<Real> v_diff; // the diffuion velocity, need to calculate the flux
-    
+
   int refinement_idx{-1};
 
   AthenaArray<Real> flux[3]; // store transport flux, also need for refinement
-  
+
   Real vmax; // the maximum velocity (effective speed of light)
+  bool vmax_Edit; 
   Real vlim;
   Real max_opacity;
 
 
   MeshBlock* pmy_block;    // ptr to MeshBlock containing this Fluid
   CellCenteredBoundaryVariable cr_bvar;
-  
+
   CRIntegrator *pcrintegrator;
-  
-  
+
+
   //Function in problem generators to update opacity
   void EnrollOpacityFunction(CROpacityFunc MyOpacityFunction);
 
@@ -72,14 +73,14 @@ public:
   CROpacityFunc UpdateOpacity;
 
 
-  AthenaArray<Real> cwidth; 
+  AthenaArray<Real> cwidth;
   AthenaArray<Real> cwidth1;
   AthenaArray<Real> cwidth2;
   AthenaArray<Real> b_grad_pc; // array to store B\dot Grad Pc
   AthenaArray<Real> b_angle; //sin\theta,cos\theta,sin\phi,cos\phi of B direction
 
   int stream_flag; // flag to include streaming or not
-  int src_flag; // flag to 
+  int src_flag; // flag to
 
 private:
 
